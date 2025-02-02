027175bae8cc519b17e00ad8ff5982e3ccc371ce
Sheridan Lugo <sheridanlugo@gmail.com>
2025-02-01 23:52:15 -0800

    asdf

---
+++ js/gsap-scrolltrigger.js
@@ -0,0 +1,6 @@
+gsap.to('large-link', {
+    y:0, 
+    stagger: 0.05,
+    delay: 0.2,
+    duration: .1,
+})
---
+++ js/swiper.js
@@ -0,0 +1,33 @@
+const swiper = new Swiper('.swiper', {
+  effect: "creative",
+  creativeEffect: {
+    prev: {
+      shadow: true,
+      translate: [0, 0, -400],
+    },
+    next: {
+      translate: ["100%", 0, 0],
+    },
+  },
+    direction: 'horizontal',
+    loop: true,
+    speed: 700,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+
+    on: {
+      click(event) {
+        console.log('clickd');
+        this.slideNext();
+      },
+    },
+
+
+  });
+
+  //swiper.slideNext
+  
